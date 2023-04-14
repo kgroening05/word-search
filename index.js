c6926962e7b62261b6a7d8c5ea9e4b907b04e095
Kyle Groening <kylegroening@gmail.com>
2023-04-14 09:54:24 -0400

    Add view settings, body parser middleware

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const readText = require('./detectText')
+const puzzleHandler = require('./utils/wordSearchHandler')
 require('dotenv').config()
 
 app.set("port", process.env.PORT || 8080)
-app.set("views", "ejs")
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'))
 
+app.post(
+    '/read',
+    puzzleHandler,
+    (req, res) => {
+        res.send('Hello World')
+})
 app.get(
     '/',
-    readText,
     (req, res) => {
-    res.send('Hello World')
-})
+        res.render('main')
+    }
+)
 
 app.listen(app.get('port'))
 
